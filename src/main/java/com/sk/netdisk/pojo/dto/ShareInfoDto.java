fc1023355561a78369aa25063fbde08b4e76f111
guaZong <lsj_sj1121@163.com>
2023-07-27 16:19:20 +0800

    fix : 删除shareNum并添加注释

--- src/main/java/com/sk/netdisk/pojo/dto/ShareInfoDto.java
+++ src/main/java/com/sk/netdisk/pojo/dto/ShareInfoDto.java
@@ -14,31 +14,56 @@ import java.util.List;
 @NoArgsConstructor
 public class ShareInfoDto {
 
-    private Integer id;
-
+    /**
+     * 分享id
+     */
+    private Integer dataShareId;
+    /**
+     * 分享链接
+     */
     private String link;
-
+    /**
+     * 提取码
+     */
     private String passCode;
-
+    /**
+     * 创建时间
+     */
     private String createTime;
-
+    /**
+     * 过期时间
+     */
     private Integer expireDays;
-
+    /**
+     * 保存次数
+     */
     private Integer saveNum;
-
-    private Integer shareNum;
-
+    /**
+     * 下载次数
+     */
     private Integer downloadNum;
-
+    /**
+     * 浏览次数
+     */
     private Integer lookNum;
-
+    /**
+     * 分享的文件id集合
+     */
     private List<Integer> dataIds;
-
+    /**
+     * 分享的文件名字集合
+     */
     private List<String> nameList;
-
+    /**
+     * 分享的文件类型集合
+     */
     private List<Integer> type;
-
+    /**
+     * 允许查看人数
+     */
     private Integer accessNum;
-
+    /**
+     * 允许查看人数状态(0是有人数限制,1是无人数限制,2是过期,3是被人删除)
+     */
     private Integer accessStatus;
 }
